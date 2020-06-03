1701b3f7c07a22c7e490e9269cf62b5db7c01fa2
Ryan Hafen <rhafen@gmail.com>
2020-06-02 17:03:25 -0700

    Update install script

--- src/install.sh
+++ src/install.sh
@@ -5,9 +5,18 @@ source "${GITHUB_WORKSPACE}/.github/scripts/shutils.sh"
 ###############################################################################
 
 installAptPackages r-cran-tidyverse
+installAptPackages r-cran-ggthemes
+installAptPackages r-cran-isoband
+installAptPackages r-cran-checkmate
+installAptPackages r-cran-diptest
+installAptPackages r-cran-mclust
+installAptPackages r-cran-webshot
+installAptPackages r-cran-autocogs
+installAptPackages r-cran-DistributionUtils
 
-R -e "remotes::install_github('covid-open-data/geoutils')"
-R -e "remotes::install_github('covid-open-data/geocard')"
-R -e "remotes::install_github('covid-open-data/casecountapp')"
+R -e "remotes::install_github('hafen/trelliscopejs@new-features', dependencies = FALSE)"
+R -e "remotes::install_github('covid-open-data/geoutils', dependencies = FALSE)"
+R -e "remotes::install_github('covid-open-data/geocard', dependencies = FALSE)"
+R -e "remotes::install_github('covid-open-data/casecountapp', dependencies = FALSE)"
 
 exit 0
